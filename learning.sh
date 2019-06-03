4208d64005605de72cd0eb34f68a4960041101cb
Marcin Wozniak <marcinwozniak@zoho.com>
2019-06-03 11:27:14 +0200

    Nauczony

--- learning.sh
+++ learning.sh
@@ -3,3 +3,4 @@
 cat sampleDatas/* > sampleDatasAll
 vw sampleDatasAll -l 10 -c --passes 100000 --holdout_off -f ./models/walking.model --holdout_off 8  
 rm -rf sampleDatasAll
+rm -rf sampleDatasAll.cache
