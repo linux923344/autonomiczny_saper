13980ee227a3552f6ea22d5a4315149af8917f4b
FilipI <filip.izydorczyk@onet.pl>
2019-06-12 09:39:53 +0200

    Skonczenie drzewka

--- modules/VowpalVabbit/TreeCreator.py
+++ modules/VowpalVabbit/TreeCreator.py
@@ -1,5 +1,9 @@
 from sklearn import tree
 from joblib import dump, load
+from sklearn.tree import export_graphviz
+from sklearn.externals.six import StringIO
+from IPython.display import Image
+import pydotplus
 
 
 class TreeCreator:
@@ -12,6 +16,8 @@ class TreeCreator:
 
     def save(self, name):
         clf = load("./models/walkingdecision.model.joblib")
+        feature_cols = ["x", "x", "x", "x", "x", "x", "x", "x", "x", "x", "x", "x", "x", "x", "x", "x", "x", "x", "x", "x", "x", "x", "x",
+                        "x", "x", "x", "x", "x", "x", "x", "x", "x", "x", "x", "x", "x", "x", "x", "x", "x", "x", "x", "x", "x", "x", "x", "x", "x", "x"]
         argumentsList = []
         resultsList = []
         for data in self.datas:
@@ -19,4 +25,13 @@ class TreeCreator:
             argumentsList.append(data.getCellsInArray())
 
         clf = clf.fit(argumentsList, resultsList)
+
+        dot_data = StringIO()
+        export_graphviz(clf, out_file=dot_data,
+                        filled=True, rounded=True,
+                        special_characters=True, feature_names=feature_cols, class_names=feature_cols)
+        graph = pydotplus.graph_from_dot_data(dot_data.getvalue())
+        graph.write_png('diabetes.png')
+        Image(graph.create_png())
+
         dump(clf, name)
